6b77c8dd390a82509a0af5923b58df8316774563
jaydeep apte <jaydeepresources@gmail.com>
2018-08-02 17:30:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+paraFlag = true;
+
+function toggle() {
+    paraFlag = !paraFlag;
+
+    if (!paraFlag) {
+        document.getElementById("paraId").style.display = "none";
+        document.getElementById("caption").innerText = "show";
+    }
+    else {
+        document.getElementById("paraId").style.display = "block";
+        document.getElementById("caption").innerText = "hide";
+    }
+}
